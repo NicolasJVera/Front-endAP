ff352da7d6dd7281c28a7a74318ce11c8e0872f9
Nicolas Vera <nico03veraa@gmail.com>
2023-06-30 22:36:29 -0300

    cambios en el service y html de acerca de y proyectos

--- src/app/model/proyectos.ts
+++ src/app/model/proyectos.ts
@@ -2,7 +2,7 @@ export class Proyecto {
   id?: number;
   nombre: string;
   descripcion: String;
-  imagen: string;
+  imagen: string = '../../../assets/img/proyecto.png';
 
   constructor(nombre: string, descripcion: string, imagen: string){
       this.nombre = nombre;
